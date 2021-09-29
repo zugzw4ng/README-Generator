4190c9b62e0729cd1ba91132c2aa89b0611b5c80
zugzw4ng <bongcloud2001@gmail.com>
2021-09-29 18:41:53 -0400

    added function to write README file

--- index.js
+++ index.js
@@ -1,65 +1,72 @@
-// TODO: Include packages needed for this application
-const inquirer = require("inquirer");
-// TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        message: "Enter your Github username",
-        name: 'username',
-        default: 'Your Username',
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-    },
-    {
-        type: 'input',
-        message: "Enter the repo name for this project",
-        name: 'repo',
-        default: 'Repository Name',
+const questions = [{
+    type: 'input',
+    message: "Enter your Github username",
+    name: 'username',
+    default: 'Your Username',
 
-    },
-    {
-        type: 'input',
-        message: "Enter the title of your project",
-        name: 'title',
-        default: 'Project Title',
+},
+{
+    type: 'input',
+    message: "Enter the repo name for your project",
+    name: 'repo',
+    default: 'Repository Name',
 
-    },
-    {
-        type: 'input',
-        message: "Enter a project description",
-        name: 'description',
-        default: 'Project Description',
+},
+{
+    type: 'input',
+    message: "Enter the title of your project",
+    name: 'title',
+    default: 'Project Title',
 
-    },
-    {
-        type: 'input',
-        message: "Enter installation instructions for this project (if installation is required)",
-        name: 'installation'
-    },
-    {
-        type: 'input',
-        message: "Enter usage information for this project",
-        name: 'usage'
-    },
-    {
-        type: 'input',
-        message: "",
-        name: 'contributing'
-    },
-    {
-        type: 'input',
-        message: "",
-        name: 'tests'
-    },
-    {
-        type: 'list',
-        message: "Choose a license for your project.",
-        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
-        name: 'license'
-    }
-];
+},
+{
+    type: 'input',
+    message: "Enter a project description",
+    name: 'description',
+    default: 'Project Description',
+
+},
+{
+    type: 'input',
+    message: "Enter installation instructions for your project (if installation is required)",
+    name: 'installation'
+},
+{
+    type: 'input',
+    message: "Enter usage information for your project",
+    name: 'usage'
+},
+{
+    type: 'input',
+    message: "Enter guidelines for contributions to your project by other developers",
+    name: 'contributions'
+},
+{
+    type: 'input',
+    message: "Describe any tests involved with your project",
+    name: 'tests'
+},
+{
+    type: 'list',
+    message: "Choose a license for your project.",
+    choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+    name: 'license'
+}];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("Your README.md file has been created")
+    });
+}
 
 // TODO: Create a function to initialize app
 function init() { }
